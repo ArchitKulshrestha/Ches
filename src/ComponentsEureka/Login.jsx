ee13dd4471f48cdc41de4738f0760b778da1448c
Archit <archit499kul@gmail.com>
2023-12-25 02:30:47 +0530

    Add show/hide password functionality in Login component

--- src/ComponentsEureka/Login.jsx
+++ src/ComponentsEureka/Login.jsx
@@ -5,12 +5,17 @@ import { styles } from "../styles";
 import { fadeIn } from "../utils/motion";
 import { useForm } from "react-hook-form";
 import AuthContext from "../Context/AuthProvider";
-import { useContext } from "react";
-
+import { useContext, useState } from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 
 const Login = () => {
+  const [showPasssword, setShowPassword] = useState(false);
+  const handlepassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   const { setAuth } = useContext(AuthContext);
 
   const { register, handleSubmit, reset } = useForm();
@@ -46,11 +51,11 @@ const Login = () => {
   };
 
   return (
-    <section className="h-screen bg-background-950  pt-20 sm:px-20 px-10">
-      <div className="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden sm:w-4/6 ">
+    <section className="h-screen bg-background-950 flex justify-center items-center">
+      <div className=" flex xl:flex-row flex-col-reverse gap-10 overflow-hidden w-[90%] sm:w-2/4">
         <motion.div
           variants={fadeIn("left", "tween", 0.2, 0.75)}
-          className="flex-[0.75] bg-background-900/70 backdrop-blur-md py-4 p-2 sm:p-6 rounded-2xl">
+          className="flex-[0.75] bg-background-900/70 backdrop-blur-md py-4 p-3 sm:p-6 rounded-2xl">
           <h3 className={styles.sectionHeadText}>Login.</h3>
           <form
             onSubmit={handleSubmit(onSubmit)}
@@ -68,15 +73,35 @@ const Login = () => {
             </label>
             <label className="flex flex-col">
               <span className="text-white font-medium mb-4">Your Password</span>
+
               <input
-                type="password"
+                type={showPasssword ? "text" : "password"}
                 name="password"
                 {...register("password")}
                 required
                 placeholder="password"
                 className="bg-text-100 py-4 px-6 placeholder:text-text-300 text-text-800 rounded-lg outline-none border-none font-medium"
               />
+              <div
+                onClick={handlepassword}
+                className="
+                right-[12%]
+                top-[68%]
+                sm:top-[70%]
+                absolute
+              text-text-800
+              font-medium
+              cursor-pointer
+              text-3xl
+              hover:text-text-700
+              transition
+              duration-200
+              ease-in-out
+              hover:underline">
+                {showPasssword ? <IoMdEyeOff /> : <IoMdEye />}
+              </div>
             </label>
+
             <button
               type="submit"
               className="bg-secondary-600 py-3 px-8 rounded-xl outline-none w-fit text-text-100 font-bold shadow-md hover:bg-secondary-800">
